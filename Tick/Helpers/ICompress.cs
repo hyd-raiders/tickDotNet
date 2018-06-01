7745394ed8da3c7712fbea5489a4f04132aad012
holyond <holyond@163.com>
2018-06-01 14:14:30 +0800

    addCompress

--- Tick/Helpers/ICompress.cs
+++ Tick/Helpers/ICompress.cs
@@ -6,6 +6,9 @@ using System.Text;
 
 namespace Tick.Helpers
 {
+    /// <summary>
+    /// 通用压缩接口
+    /// </summary>
     public interface ICompress
     {
         /// <summary>
@@ -13,14 +16,14 @@ namespace Tick.Helpers
         /// </summary>
         /// <param name="data">数据字节流</param>
         /// <returns>压缩后的数据</returns>
-        public byte[] CompressData(byte[] data);
+        byte[] CompressData(byte[] data);
 
         /// <summary>
         /// 压缩数据
         /// </summary>
         /// <param name="data">数据</param>
         /// <returns>压缩后数据</returns>
-        public string CompressData(string data);
+        string CompressData(string data);
 
         /// <summary>
         /// 大文件压缩
@@ -28,14 +31,14 @@ namespace Tick.Helpers
         /// <param name="inStream">入流</param>
         /// <param name="outStream">出流</param>
         /// <returns></returns>
-        public void CompressData(Stream inStream, Stream outStream);
+        void CompressData(Stream inStream, Stream outStream);
         /// <summary>
         /// 压缩文件，支持大文件压缩
         /// </summary>
         /// <param name="srcFile">源文件</param>
         /// <param name="tgtFile">目标文件</param>
         /// <returns></returns>
-        public void CompressData(string srcFile, string tgtFile);
+        void CompressData(string srcFile, string tgtFile);
 
         /// <summary>
         /// 解压文件，支持大文件压缩
@@ -43,7 +46,7 @@ namespace Tick.Helpers
         /// <param name="srcFile">源文件</param>
         /// <param name="tgtFile">目标文件</param>
         /// <returns></returns>
-        public void DeCompressData(string srcFile, string tgtFile);
+        void DeCompressData(string srcFile, string tgtFile);
 
         /// <summary>
         /// 解压数据
@@ -51,14 +54,14 @@ namespace Tick.Helpers
         /// <param name="inStream">入流</param>
         /// <param name="outStream">出流</param>
         /// <returns>解压后数据</returns>
-        public void DeCompressData(Stream inStream, Stream outStream);
+        void DeCompressData(Stream inStream, Stream outStream);
 
         /// <summary>
         /// 解压数据
         /// </summary>
         /// <param name="data">数据字节数组</param>
         /// <returns>解压后数据</returns>
-        public byte[] DeCompressData(byte[] data);
+        byte[] DeCompressData(byte[] data);
 
 
 
@@ -67,6 +70,6 @@ namespace Tick.Helpers
         /// </summary>
         /// <param name="data">数据/param>
         /// <returns>解压后数据</returns>
-        public string DeCompressData(string data);
+        string DeCompressData(string data);
     }
 }
